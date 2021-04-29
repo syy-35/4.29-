1f7f8f4261e4033b1d7e2decf4888764e2270f74
邓伦女友 <8899684+syy-hh@user.noreply.gitee.com>
2021-04-29 20:07:51 +0800

    项目模板

--- demo/src/router/index.js
+++ demo/src/router/index.js
@@ -8,7 +8,28 @@ const routes = [
     path:"/",
     name:"home",
     component:()=>import("../views/Home.vue")
-  }
+  },
+  {
+    path:"/two",
+    name:"two",
+    component:()=>import("../views/Two.vue")
+  },
+  {
+    path:"/three",
+    name:"three",
+    component:()=>import("../views/Three.vue")
+  },
+  {
+    path:"/four",
+    name:"four",
+    component:()=>import("../views/Four.vue")
+  },
+  {
+    path:"/six",
+    name:"six",
+    component:()=>import("../views/Six.vue")
+  },
+
 ];
 
 const router = new VueRouter({
