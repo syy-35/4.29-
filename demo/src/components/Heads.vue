c21bc83cee8eadd8443abc61770564f80474aad6
邓伦女友 <8899684+syy-hh@user.noreply.gitee.com>
2021-04-30 08:57:59 +0800

    4.30上午模板

---
+++ demo/src/components/Heads.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <ul>
+      <li><img src="@/assets/img/切图/logo.png" alt=""></li>
+      <li class="center">
+        <slot></slot>
+      </li>
+      <li>我的</li>
+      <li>导航</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  },
+}
+</script>
+
+<style scoped>
+ul {
+  display: flex;
+  justify-content: space-between;
+  font-size: .3rem;
+  padding: 0 .2rem;
+}
+ul li {
+  height: 1rem;
+  line-height: 1rem;
+  color: grey;
+}
+.center {
+  width: 30%;
+  border: 1px solid black;
+  font-size: .5rem;
+  text-align: center;
+  color: black;
+}
+ul li img {
+  width: 2rem;
+  vertical-align: middle;
+}
+</style>
