a5fecbad28e827dab29d0764d429f84e99643783
邓伦女友 <8899684+syy-hh@user.noreply.gitee.com>
2021-05-04 19:40:37 +0800

    申偃亿最后提交

--- demo/src/views/Shijiu.vue
+++ demo/src/views/Shijiu.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
+    <Heads>关于我们</Heads>
+    <img src="@/assets/img/syy/tou.jpg" alt="">
+    <h3>房先森简介</h3>
+    <p>
+      房先森互联网装修平台由杭州瑞壹网络科技有限公司于2015年成立上线，房先森立足刚需房，专注于家庭装修服务，
+      利用互联网将装修、供应链、金融进行融合，多维度实现用户对设计师、工长、工地、材料等的线上选择体验，房
+      先森以自营的模式真正意义上过滤掉传统家装公司利润，将利润让给业主。房先森与银行合作的专项装修贷款能够
+      解决业主装修资金问题，并以装修套餐+个性化定制的模式，提供创新型装修服务。
+    </p>
 
+    <div class="main">
+      <h3>我们是谁，我们如何与众不同？</h3>
+      <img class="foot" src="@/assets/img/syy/foot.jpg" alt="">
+    </div>
   </div>
 </template>
 
@@ -20,5 +33,20 @@ export default {
 </script>
 
 <style scoped>
-
+img {
+  width: 100%;
+}
+h3 {
+  font-size: .4rem;
+  text-align: center;
+  font-weight: 400;
+}
+p {
+  font-size: .3rem;
+  padding: .2rem;
+}
+.main {
+  background-color: #F4F4F4;
+  padding: .2rem 0;
+}
 </style>
--- demo/src/views/Shiwu.vue
+++ demo/src/views/Shiwu.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-
+    <Heads>装修报价</Heads>
+    <img class="tou" src="@/assets/img/切图/10quoted.jpg" alt="">
+    <h3>房先森10s报价</h3>
+    <img src="@/assets/img/syy/input.jpg" alt="">
+    <img src="@/assets/img/syy/five.jpg" alt="">
   </div>
 </template>
 
@@ -20,5 +24,12 @@ export default {
 </script>
 
 <style scoped>
-
+img {
+  width: 100%;
+}
+h3 {
+  font-size: .4rem;
+  text-align: center;
+  font-weight: 400;
+}
 </style>
